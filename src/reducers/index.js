8a4758d2c5914e1463a4112c71fa0555f5b491cc
srikanth4346 <srikanth4346@gmail.com>
2017-04-30 01:41:54 -0700

    React Reducx CRUP App

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import ReducersPosts from './reducers_posts';
+import {reducer as formReducer} from 'redux-form';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  posts: ReducersPosts,
+  form:formReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/reducers_posts.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Srikanth on 4/24/2017.
+ */
+import {FETCH_POSTS, FETCH_POST, DELETE_POST} from '../actions/index';
+const INITIAL_STATE ={all:[], post:null};
+
+export default function (state= INITIAL_STATE, action){
+  switch (action.type){
+      case FETCH_POSTS:
+          return {...state, all: action.payload.data};
+      case FETCH_POST:
+          return {...state, post:action.payload.data};
+      case DELETE_POST:
+          return {...state, delete:action.payload.data};
+      default:
+          return state;
+
+  }
+}
